37e2c8d21a3b6cecc50ae41c19e905ab6dff8a61
quocanhvu16 <quocanhvu16@gmail.com>
2023-01-17 16:00:12 +0700

    Update vqanh_20194228

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './Reducer';
+import {BrowserRouter as Router}from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const render =() => root.render(
 // root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>
   /* </React.StrictMode> */
 );
